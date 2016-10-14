1bbb223d2e6c03362163caa5640d010d9462c525
nitkna <nithin.krishna@outlook.com>
2016-10-14 15:34:24 +0530

    adds post delete functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -35,6 +35,12 @@ class PostsController < ApplicationController
     end
   end
 
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to root_path
+  end
+
   private
   def params_post
     params.require(:post).permit(:title, :body)
